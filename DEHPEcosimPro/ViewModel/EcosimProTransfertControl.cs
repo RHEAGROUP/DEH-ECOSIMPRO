750598a936fbe15e6c95aa61ee1b9a98bd038d2b
nathanael smiechowski <n.smiechowski@rheagroup.com>
2021-01-27 16:41:20 +0100

    Net change preview; fixes #9

---
+++ DEHPEcosimPro/ViewModel/EcosimProTransfertControl.cs
@@ -0,0 +1,92 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="EcosimProTransferControlViewModel.cs" company="RHEA System S.A.">
+//    Copyright (c) 2020-2021 RHEA System S.A.
+// 
+//    Author: Sam Gerené, Alex Vorobiev, Alexander van Delft, Nathanael Smiechowski.
+// 
+//    This file is part of DEHPEcosimPro
+// 
+//    The DEHPEcosimPro is free software; you can redistribute it and/or
+//    modify it under the terms of the GNU Lesser General Public
+//    License as published by the Free Software Foundation; either
+//    version 3 of the License, or (at your option) any later version.
+// 
+//    The DEHPEcosimPro is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//    Lesser General Public License for more details.
+// 
+//    You should have received a copy of the GNU Lesser General Public License
+//    along with this program; if not, write to the Free Software Foundation,
+//    Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace DEHPEcosimPro.ViewModel
+{
+    using System;
+    using System.Reactive;
+    using System.Threading.Tasks;
+
+    using DEHPCommon.UserInterfaces.ViewModels;
+
+    using DEHPEcosimPro.DstController;
+
+    using ReactiveUI;
+
+    /// <summary>
+    /// <inheritdoc cref="TransferControlViewModel"/>
+    /// </summary>
+    public class EcosimProTransferControlViewModel : TransferControlViewModel
+    {
+        /// <summary>
+        /// The <see cref="IDstController"/>
+        /// </summary>
+        private readonly IDstController dstController;
+
+        /// <summary>
+        /// Backing field for <see cref="AreThereAnyTransferInProgress"/>
+        /// </summary>
+        private bool areThereAnyTransferInProgress;
+
+        /// <summary>
+        /// Gets or sets a value indicating whether the TransfertCommand" is executing
+        /// </summary>
+        public bool AreThereAnyTransferInProgress
+        {
+            get => this.areThereAnyTransferInProgress;
+            set => this.RaiseAndSetIfChanged(ref this.areThereAnyTransferInProgress, value);
+        }
+
+        /// <summary>
+        /// Initializes a new <see cref="EcosimProTransferControlViewModel"/>
+        /// </summary>
+        /// <param name="dstController">The <see cref="IDstController"/></param>
+        public EcosimProTransferControlViewModel(IDstController dstController)
+        {
+            this.dstController = dstController;
+            var canTransfert = this.WhenAnyValue(x => x.dstController.HasSomeMappedThingsReadyToTransfert);
+            this.TransferCommand = ReactiveCommand.CreateAsyncTask(canTransfert, async _ => await this.TransfertCommandExecute());
+            var canCancel = this.WhenAnyValue(x => x.AreThereAnyTransferInProgress);
+            this.CancelCommand = ReactiveCommand.CreateAsyncTask(canCancel, async _ => await this.CancelTransfer());
+        }
+
+        /// <summary>
+        /// Cancels the transfer in progress
+        /// </summary>
+        /// <returns>A <see cref="Task"/><returns>
+        private Task CancelTransfer()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Executes the transfert command
+        /// </summary>
+        /// <returns>A <see cref="Task"/></returns>
+        private async Task TransfertCommandExecute()
+        {
+            await this.dstController.Transfer();
+        }
+    }
+}
---
+++ DEHPEcosimPro/ViewModel/NetChangePreview/EcosimProNetChangePreviewViewModel.cs
@@ -0,0 +1,145 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="EcosimProNetChangePreviewViewModel.cs" company="RHEA System S.A.">
+//    Copyright (c) 2020-2021 RHEA System S.A.
+// 
+//    Author: Sam Gerené, Alex Vorobiev, Alexander van Delft, Nathanael Smiechowski.
+// 
+//    This file is part of DEHPEcosimPro
+// 
+//    The DEHPEcosimPro is free software; you can redistribute it and/or
+//    modify it under the terms of the GNU Lesser General Public
+//    License as published by the Free Software Foundation; either
+//    version 3 of the License, or (at your option) any later version.
+// 
+//    The DEHPEcosimPro is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//    Lesser General Public License for more details.
+// 
+//    You should have received a copy of the GNU Lesser General Public License
+//    along with this program; if not, write to the Free Software Foundation,
+//    Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace DEHPEcosimPro.ViewModel.NetChangePreview
+{
+    using System;
+    using System.Linq;
+    using System.Reactive.Linq;
+
+    using CDP4Dal;
+
+    using DEHPCommon.Events;
+    using DEHPCommon.HubController.Interfaces;
+    using DEHPCommon.Services.ObjectBrowserTreeSelectorService;
+    using DEHPCommon.UserInterfaces.ViewModels;
+    using DEHPCommon.UserInterfaces.ViewModels.NetChangePreview;
+    using DEHPCommon.UserInterfaces.ViewModels.Rows.ElementDefinitionTreeRows;
+
+    using DEHPEcosimPro.DstController;
+    using DEHPEcosimPro.ViewModel.Interfaces;
+
+    using ReactiveUI;
+
+    public class EcosimProNetChangePreviewViewModel : NetChangePreviewViewModel, IEcosimProNetChangePreviewViewModel
+    {
+        /// <summary>
+        /// The <see cref="IDstController"/>
+        /// </summary>
+        private readonly IDstController dstController;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:DEHPCommon.UserInterfaces.ViewModels.ObjectBrowserViewModel" /> class.
+        /// </summary>
+        /// <param name="hubController">The <see cref="T:DEHPCommon.HubController.Interfaces.IHubController" /></param>
+        /// <param name="objectBrowserTreeSelectorService">The <see cref="T:DEHPCommon.Services.ObjectBrowserTreeSelectorService.IObjectBrowserTreeSelectorService" /></param>
+        /// <param name="dstController">The <see cref="IDstController"/></param>
+        public EcosimProNetChangePreviewViewModel(IHubController hubController, IObjectBrowserTreeSelectorService objectBrowserTreeSelectorService, IDstController dstController) : base(hubController, objectBrowserTreeSelectorService)
+        {
+            this.dstController = dstController;
+
+            this.WhenAnyValue(x => x.dstController.HasSomeMappedThingsReadyToTransfert)
+                .Skip(1)
+                .ObserveOn(RxApp.MainThreadScheduler)
+                .Subscribe(this.UpdateTree);
+        }
+
+        /// <summary>
+        /// Updates the tree
+        /// </summary>
+        /// <param name="shouldShowPreview">A value indicating whether the tree should show a preview</param>
+        public void UpdateTree(bool shouldShowPreview)
+        {
+            if (shouldShowPreview)
+            {
+                this.ComputeValues();
+            }
+            else
+            {
+                this.Reload(true);
+            }
+        }
+
+        /// <summary>
+        /// Computes the old values for each <see cref="P:DEHPCommon.UserInterfaces.ViewModels.ObjectBrowserViewModel.Things" />
+        /// </summary>
+        public override void ComputeValues()
+        {
+            foreach (var iterationRow in this.Things.OfType<ElementDefinitionsBrowserViewModel>())
+            {
+                foreach (var thing in this.dstController.ElementDefinitionParametersDstVariablesMaps)
+                {
+                    var elementToUpdate = iterationRow.ContainedRows.OfType<ElementDefinitionRowViewModel>()
+                        .FirstOrDefault(x => x.Thing.Iid == thing.Iid);
+
+                    if (elementToUpdate is {})
+                    {
+                        if (!thing.Parameter.All(p => elementToUpdate.Thing.Parameter.Any(x => x.Iid == p.Iid)))
+                        {
+                            thing.Parameter.AddRange(elementToUpdate.Thing.Parameter.Where(x => thing.Parameter.All(p => p.Iid != x.Iid)));
+                        }
+
+                        CDPMessageBus.Current.SendMessage(new HighlightEvent(elementToUpdate.Thing), elementToUpdate.Thing);
+                        elementToUpdate.UpdateThing(thing);
+                        elementToUpdate.UpdateChildren();
+                    }
+                    else
+                    {
+                        iterationRow.ContainedRows.Add(new ElementDefinitionRowViewModel(thing, this.HubController.CurrentDomainOfExpertise, this.HubController.Session, iterationRow));
+                        CDPMessageBus.Current.SendMessage(new HighlightEvent(thing), thing);
+                    }
+
+                    foreach (var elementUsage in thing.ContainedElement)
+                    {
+                        var elementUsageToUpdate = iterationRow.ContainedRows.OfType<ElementDefinitionRowViewModel>()
+                            .SelectMany(x => x.ContainedRows.OfType<ElementUsageRowViewModel>())
+                            .FirstOrDefault(x => x.Thing.Iid == elementUsage.Iid);
+
+                        if (elementUsageToUpdate is null)
+                        {
+                            continue;
+                        }
+
+                        if (!elementUsage.ParameterOverride.All(p => elementUsageToUpdate.Thing.ParameterOverride.Any(x => x.Iid == p.Iid)))
+                        {
+                            elementUsage.ParameterOverride.AddRange(elementUsageToUpdate.Thing.ParameterOverride.Where(x => thing.Parameter.All(p => p.Iid != x.Iid)));
+                        }
+
+                        CDPMessageBus.Current.SendMessage(new HighlightEvent(elementUsageToUpdate.Thing), elementUsageToUpdate.Thing);
+                        elementUsageToUpdate.UpdateThing(elementUsage);
+                        elementUsageToUpdate.UpdateChildren();
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Not available for the nset change preview panel
+        /// </summary>
+        public override void PopulateContextMenu()
+        {
+            this.ContextMenu.Clear();
+        }
+    }
+}
